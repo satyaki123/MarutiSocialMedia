7ab5315a58791ee766407bf4f0210cfc1fd7e2f4
satyaki.nmiet@gmail.com <satyaki.nmiet@gmail.com>
2015-02-09 13:26:18 +0530

    Resolved fonts issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 #Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
 # Rails.application.config.assets.precompile += ['jquery.dataTables.min.js',' dataTables.bootstrap.js',' posts.js','dataTables.bootstrap.css','style.css' ]
-Rails.application.config.assets.precompile += ['bootstrap.min.js','dataTables.bootstrap.js','posts.js','dataTables.bootstrap.css','font-awesome.css','bootstrap.min.css','jquery.js','jquery_ujs.js','jquery.dataTables.min.js','font-awesome.min.css','dashboard.css','.svg ','.eot','.woff ','.ttf']
+Rails.application.config.assets.precompile += ['bootstrap.min.js','dataTables.bootstrap.js','posts.js','dataTables.bootstrap.css','font-awesome.css','bootstrap.min.css','jquery.js','jquery_ujs.js','jquery.dataTables.min.js','font-awesome.min.css','dashboard.css']
